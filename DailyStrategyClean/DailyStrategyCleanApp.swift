043a46311afdb31b49d12ea87ea26180cde12336
P <ptleung@gmail.com>
2026-05-31 21:51:02 -0500

    Initial Commit

---
+++ DailyStrategyClean/DailyStrategyCleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyStrategyCleanApp.swift
+//  DailyStrategyClean
+//
+//  Created by Pak Leung on 5/31/26.
+//
+
+import SwiftUI
+
+@main
+struct DailyStrategyCleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
